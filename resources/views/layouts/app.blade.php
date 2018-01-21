840b39892c56d67a431e002190ca00f489c14b11
kenware <ejykken@gmail.com>
2018-01-21 12:37:43 +0100

    new changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,17 @@
 
     <!-- Styles -->
     
-    <script src="{{asset('js/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('js/jquery/popper.js')}}"></script>
-    <script src="{{asset('js/custom.js')}}"></script>
-    <link href=" {{asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href=" {{asset('css/main.css') }}" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
     
     <style>
@@ -79,9 +85,6 @@
 
  
     <!-- JavaScripts -->
-    <script src="{{asset('js/bootstrap/bootstrap.min.js')}}"></script>
-    <script src="{{asset('js/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('js/jquery/popper.js')}}"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
