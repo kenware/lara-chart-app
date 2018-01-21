c0b2b7b6775eb28dbe0102365685de5927fe0f79
kenware <ejykken@gmail.com>
2018-01-21 13:01:46 +0100

    new changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,22 +12,19 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
     <!-- Styles -->
-    <script src="{{asset('js/custom.js')}}"></script>
-    <link href=" {{asset('css/main.css') }}" rel="stylesheet">
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-    
-    <style>
+        <style>
         body {
           
             font-family: 'Lato';
@@ -36,7 +33,26 @@
         .fa-btn {
             margin-right: 6px;
         }
-    </style>
+        .room{
+        background-color: aliceblue;
+        }
+        .welcome{
+          margin-top: 5rem;
+        }
+        .sidebar {
+          background-color: rgb(198, 235, 222);
+          color: white;
+          top:50px;
+          position: fixed;
+          bottom: 0;
+          left: 0;
+          z-index: 1000;
+          padding: 20px 0;
+          overflow-x: hidden;
+          overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
+          border-right: 1px solid #eee;
+        }
+          </style>
 </head>
 <body id="app-layout">
 
@@ -86,6 +102,18 @@
 
  
     <!-- JavaScripts -->
+    <script>
+    $(document).ready(function () {
+      
+        $('#chart').hide();
+        $('#add').click(function(){
+            $('#chart').toggle(); 
+        });
+        $('#ad').click(function(){
+            $('#chart').toggle(); 
+        });
+    });
+    </script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
