b2a3400d33a720900e6c25ff6332aa6ef08f2804
kenware <ejykken@gmail.com>
2018-01-24 01:08:53 +0100

    changes

--- config/cloudder.php
+++ config/cloudder.php
@@ -12,12 +12,12 @@ return [
     |
     */
 
-    'cloudName'  => env('CLOUDINARY_CLOUD_NAME'),
+    'cloudName'  => 'more-recipes',
     'baseUrl'    => env('CLOUDINARY_BASE_URL', 'http://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
     'secureUrl'  => env('CLOUDINARY_SECURE_URL', 'https://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
     'apiBaseUrl' => env('CLOUDINARY_API_BASE_URL', 'https://api.cloudinary.com/v1_1/'.env('CLOUDINARY_CLOUD_NAME')),
-    'apiKey'     => env('CLOUDINARY_API_KEY'),
-    'apiSecret'  => env('CLOUDINARY_API_SECRET'),
+    'apiKey'     => '127278553653283',
+    'apiSecret'  => 'XUBlnwpJ2dbSHJzPZu-vTWxgob4',
 
     'scaling'    => [
         'format' => 'png',
